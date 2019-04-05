b9362d6167706609ccdd1613672284b1ec8caaa1
Gabriel <ti2@drlavatudo.com>
2019-04-05 02:26:25 -0300

    gogo

--- phpMVC/App/Controllers/HomeController.php
+++ phpMVC/App/Controllers/HomeController.php
@@ -10,6 +10,6 @@ class HomeController extends Controller
     public function index()
     {
         /// precisa ser mudado para pagina home , e validado caso usario nao estiver logado no session retorna a pagina de login
-        $this->render('home/home');
+        $this->render('login/Login');
     }
 }
