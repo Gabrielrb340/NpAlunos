eb15a7f06b165b9a3d3258a082341d26dac9aaa3
Jordan <jordaneduardocl@gmail.com>
2019-04-05 19:41:10 -0300

    tt

--- phpMVC/App/Controllers/CadastroController.php
+++ phpMVC/App/Controllers/CadastroController.php
@@ -20,7 +20,7 @@ class CadastroController extends Controller
         $Usuario->setEmail($_POST['email']);
         $Usuario->setSenha(md5($_POST['senha']));
         $Usuario->setTel($_POST['tel']);
-        $Usuario->setCPF($_POST['numCPF']);
+        $Usuario->setCPF($_POST['cpf']);
         $Usuario->setSetor($_POST['setor']);
         //echo $Usuario->toString();
 
@@ -33,7 +33,7 @@ class CadastroController extends Controller
         //}
 
         if($Usuario->salvar()){
-           $this->redirect('/login/Login');
+           $this->render('/cadastro/cadastrar');
         }else{
             Sessao::gravaMensagem("Erro ao gravar");
         }
--- phpMVC/App/Controllers/HomeController.php
+++ phpMVC/App/Controllers/HomeController.php
@@ -10,6 +10,11 @@ class HomeController extends Controller
     public function index()
     {
         /// precisa ser mudado para pagina home , e validado caso usario nao estiver logado no session retorna a pagina de login
-        $this->render('home/home');
+        $this->render('login/Login');
+    }
+    public function home()
+    {
+        /// precisa ser mudado para pagina home , e validado caso usario nao estiver logado no session retorna a pagina de login
+        $this->render('Home/home');
     }
 }
